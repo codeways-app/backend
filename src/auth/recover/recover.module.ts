811a5da618704e3eaf156497f370480f42944ff0
spasontis <smxxtdzmail@gmail.com>
2025-12-04 17:01:39 +0700

    feat(auth): add recover resource

--- src/auth/recover/recover.module.ts
+++ src/auth/recover/recover.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+
+import { MailService } from '../../libs/mail';
 import { UserService } from '../../user';
 import { EmailConfirmationService } from '../email-confirmation';
 import { RecoverService } from './recover.service';
 
 @Module({
-  providers: [RecoverService, EmailConfirmationService, UserService],
+  providers: [
+    RecoverService,
+    EmailConfirmationService,
+    UserService,
+    MailService,
+  ],
 })
 export class RecoverModule {}
