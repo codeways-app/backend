1283be0e95db40ce441a6638c23c8486bb523a1d
spasontis <smxxtdzmail@gmail.com>
2026-02-12 19:49:07 +0700

    feat(chat): add chats logic
    **Description**
     - Added findMember function

--- src/chat/events.gateway.ts
+++ src/chat/events.gateway.ts
@@ -2,18 +2,23 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  WsException,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { UseGuards } from '@nestjs/common';
 import { WsJwtGuard } from './ws-jwt/ws-jwt.guard';
 import { SocketAuthMiddleware } from './ws-jwt/ws.mw';
 import { SessionService } from '../session/session.service';
-import { AuthenticatedSocket } from './types/ws.types';
+import type { AuthenticatedSocket } from './types/ws.types';
+import { EventsService } from './events.service';
 
 @UseGuards(WsJwtGuard)
 @WebSocketGateway(3001, {})
 export class EventsGateway {
-  constructor(private readonly sessionService: SessionService) {}
+  constructor(
+    private readonly sessionService: SessionService,
+    private readonly eventsService: EventsService,
+  ) {}
 
   @WebSocketServer() server: Server;
 
@@ -23,31 +28,40 @@ export class EventsGateway {
   // USER CONNECTION
   handleConnection(client: AuthenticatedSocket) {
     console.log(
-      'User joined:',
+      'New Websocket connection:',
       '\n id:',
       client.id,
       '\n user:',
-      client.data.user,
+      client.data.user.login,
     );
-
-    this.server
-      .to('chat')
-      .emit('user-joined', `New User joined the chat: ${client.id}`);
   }
 
   // USER DISCONNECTION
   handleDisconnect(client: Socket) {
-    console.log('User left:', client.id);
-
-    this.server.emit('user-left', `User left the chat: ${client.id}`);
+    console.log('Websocket disconnected:', client.id);
   }
 
-  // NEW MESSAGE
-  @SubscribeMessage('newMessage')
-  handleMessage(client: Socket, message: any) {
-    console.log(client.id, message);
-    client.emit('reply', 'delivered');
+  // JOIN ROOM
+  @SubscribeMessage('joinRoom')
+  async handleJoinRoom(client: AuthenticatedSocket, room: string) {
+    console.log(room);
+    const member = await this.eventsService.findMember(
+      room,
+      client.data.user.sub,
+    );
 
-    this.server.emit('broadcasting', 'broadcasting...');
+    if (!member) {
+      console.log('Access denied: not a chat member');
+      throw new WsException('Access denied: not a chat member');
+    }
+
+    console.log(
+      'User joined room:',
+      '\n user:',
+      client.data.user.login,
+      '\n room:',
+      room,
+    );
+    await client.join(room);
   }
 }
--- src/chat/events.service.ts
+++ src/chat/events.service.ts
@@ -4,4 +4,13 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class EventsService {
   public constructor(private readonly prismaService: PrismaService) {}
+
+  public async findMember(chatId: string, userId: string) {
+    return this.prismaService.chatMember.findFirst({
+      where: {
+        chatId,
+        userId,
+      },
+    });
+  }
 }
